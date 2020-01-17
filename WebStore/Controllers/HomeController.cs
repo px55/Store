990a42c7a67ec7e7bc56f2f77f4e0b06d3a78814
Pang Xiong <pangxiong93@gmail.com>
2020-01-16 21:45:18 -0800

    test

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            workerModel workerModelView = new workerModel();
+            workerModelView = workerModelView.returnWorkerID();
+            List<workerModel> test = new List<workerModel>();
+
+            test = workerModelView.getAccounts();
+            return PartialView("_details", workerModelView);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebStore/Models/workerModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebStore.Models
+{
+    public class workerModel
+    {
+        public int worker_ID { get; set; }
+        public string worker_firstNM { get; set; }
+        public string worker_lastNM { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public int user_ID { get; set; }
+
+        public workerModel returnWorkerID()
+        {
+            workerModel newWorkerModel = new workerModel();
+
+            using (var dbContext = new StoreEntities())
+            {
+                var firstNM = (from w in dbContext.workerInfoes
+                               select w.firstNM).FirstOrDefault();
+
+                newWorkerModel.worker_firstNM = firstNM;
+
+                return newWorkerModel;
+            }
+        }
+
+        public List<workerModel> getAccounts()
+        {
+            List<workerModel> newWorkerList = new List<workerModel>();
+            workerModel newWorkerModel = new workerModel();
+
+            using (var dbContext = new StoreEntities())
+            {
+                var query = (from a in dbContext.workerAccounts
+                             select a.username).ToList();
+
+                var passwordQuery = (from a in dbContext.workerAccounts
+                                     select a.password).ToList();
+
+                foreach(var item in query)
+                {
+                    newWorkerModel.username = item;
+                    newWorkerList.Add(newWorkerModel);
+                }
+
+                return newWorkerList;
+            }
+        }
+    }
+}
